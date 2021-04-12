77d53619c68698544c08e1888101a180064a5a37
riandougherty-smoothstack <81999289+riandougherty-smoothstack@users.noreply.github.comuser.email>
2021-04-12 17:05:47 -0400

    Rename methods

--- src/com/ss/riandougherty/eval/week_one/ListOperations.java
+++ src/com/ss/riandougherty/eval/week_one/ListOperations.java
@@ -10,7 +10,7 @@ public final class ListOperations {
 	/**
 	 * Gets least-significant digit (rightmost digit).
 	 */
-	public static List<Integer> toRightMost(final Collection<Integer> input) {
+	public static List<Integer> rightDigit(final Collection<Integer> input) {
 		return input.stream()
 				// getting rightmost digit is same as calculating remainder of division by 10.
 				.map(num -> num % 10).
@@ -20,7 +20,7 @@ public final class ListOperations {
 	/**
 	 * Multiplies each element in list by 2.
 	 */
-	public static List<Integer> multByTwo(final Collection<Integer> input) {
+	public static List<Integer> doubling(final Collection<Integer> input) {
 		return input.stream()
 				// left bitshift by 1 is the same as multiplication by 2, except much faster.
 				.map(num -> num << 1)
@@ -30,7 +30,7 @@ public final class ListOperations {
 	/**
 	 * Removes all cases of 'x' in input list. 
 	 */
-	public static List<String> removeX(final Collection<String> input) {
+	public static List<String> noX(final Collection<String> input) {
 		return input.stream()
 				.map(str -> str.replaceAll("x", ""))
 				.collect(Collectors.toList());
@@ -39,7 +39,7 @@ public final class ListOperations {
 	/**
 	 * Returns true if input list can sum to 10 from elements in sequential order.
 	 */
-	public static boolean canSumSequentially(final List<Integer> input) {
+	public static boolean groupSumClump(final List<Integer> input) {
 		int firstNumIndex, sum, a;
 		
 		firstNumIndex = sum = 0;
